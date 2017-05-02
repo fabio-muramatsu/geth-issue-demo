fdfc50106ad43907d26789ba4c76332b7d2997e8
Fabio Muramatsu <fabio.muramatsu@outlook.com>
2017-05-02 09:15:46 -0300

    Added comments to scripts

--- async.js
+++ async.js
@@ -7,8 +7,21 @@ const BigNumber = require('bignumber.js');
 
 const provider = new web3.providers.HttpProvider('http://localhost:8080');
 
+/*
+    This script attempts to send 100 transactions in a row to geth, using the sendRawTransaction
+    RPC call asynchronously. In my tests, geth seems to receive all transactions, but some of them
+    are not added to the transaction pool.
+*/
+
+// Transactions are signed before sending to geth. The following private key is used, which corresponds
+// to public address 0x722bbc43bb665a5570640b0a56af48364eaba495.
+var privKey = Buffer.from("fdd97775a609e59f0fe5c8e8e24b847848c322c0ffd8f5d0ba8fe2120584ace7", "hex");
+
 var signAndSendTransaction = function(provider, nonce, callback) {
     web3.setProvider(provider);
+    
+    // Assemble a simple ether transfer transaction. Address 0x722bbc43bb665a5570640b0a56af48364eaba495
+    // should have some ethers for this to work.
     var transactionObj = {
         nonce: nonce,
         from: "0x722bbc43bb665a5570640b0a56af48364eaba495",
@@ -18,13 +31,12 @@ var signAndSendTransaction = function(provider, nonce, callback) {
         gasLimit: "0x200000"
     };
 
-    var privKey = Buffer.from("fdd97775a609e59f0fe5c8e8e24b847848c322c0ffd8f5d0ba8fe2120584ace7", "hex");
-    //Corresponding address: 0x722bbc43bb665a5570640b0a56af48364eaba495
-
+    // Sign the transaction with the private key of the sender
     var tx = new Tx(transactionObj);
     tx.sign(privKey);
     var serialized = tx.serialize();
 
+    // Send the transaction asynchronously
     web3.eth.sendRawTransaction("0x" + serialized.toString("hex"), (err, result) => {
         if(err) {
             callback({ok: false, errorMessage: err.message});
@@ -34,9 +46,10 @@ var signAndSendTransaction = function(provider, nonce, callback) {
     });         
 };
 
-var count = 1;
 var cb = (result) => console.log(result);
 
+// Send 100 transaction in a row to geth. Notice that sequential nonces are defined for the transaction,
+// so they should be considered valid by geth.
 for(let nonce = 0 ; nonce < 100 ; ++nonce) {
     signAndSendTransaction(provider, nonce, cb);
 }
--- sync.js
+++ sync.js
@@ -7,6 +7,13 @@ const BigNumber = require('bignumber.js');
 
 const provider = new web3.providers.HttpProvider('http://localhost:8080');
 
+/*
+    This script does the same thing as 'async.js', but sendRawTransaction is called synchronously.
+    In my tests, all transactions are added to the transaction pool, as expected.
+*/
+
+var privKey = Buffer.from("fdd97775a609e59f0fe5c8e8e24b847848c322c0ffd8f5d0ba8fe2120584ace7", "hex");
+
 var signAndSendTransaction = function(provider, nonce) {
     web3.setProvider(provider);
     var transactionObj = {
@@ -18,8 +25,6 @@ var signAndSendTransaction = function(provider, nonce) {
         gasLimit: "0x200000"
     };
 
-    var privKey = Buffer.from("fdd97775a609e59f0fe5c8e8e24b847848c322c0ffd8f5d0ba8fe2120584ace7", "hex");
-    //Corresponding address: 0x722bbc43bb665a5570640b0a56af48364eaba495
 
     var tx = new Tx(transactionObj);
     tx.sign(privKey);
